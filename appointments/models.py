fdcdaefd6d4e222407b22cdc82fc224d99c1a7fc
Eimantas Pauzuolis <u1363000@hud.ac.uk>
2018-01-20 14:51:06 +0000

    Created appointment model and migration

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Appointment(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    time = models.DateTimeField()
+    location = models.CharField(max_length=50)
+    patient_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=datetime.now)
--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Appointment
 # Create your views here.
+
+def index(request):
+
+    appointments = Appointment.objects.all()
+    context = {
+        'appointments': appointments
+    }
+    return render(request, 'index.html')
