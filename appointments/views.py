2163001d338df028469f31e4bebbaf94d1b7e7ae
Eimantas Pauzuolis <u1363000@hud.ac.uk>
2018-01-20 15:23:41 +0000

    Added functionality to create an appointment.

--- appointments/views.py
+++ appointments/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import Appointment
 # Create your views here.
@@ -9,4 +10,19 @@ def index(request):
     context = {
         'appointments': appointments
     }
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
+
+def add(request):
+    if(request.method == "GET"):
+        return render(request,'create.html')
+
+    if(request.method == "POST"):
+        title = request.POST['title']
+        description = request.POST['description']
+        time = request.POST['time']
+        location = request.POST['location']
+        patientName = request.POST['patientName']
+
+        appointment = Appointment(title=title, description=description, time=time, location=location, patient_name=patientName)
+        appointment.save()
+        return redirect('/')
